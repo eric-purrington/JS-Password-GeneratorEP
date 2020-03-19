e34772e539f7fbf6606aa2d7043654e989b25c7f
Seth Purr <eric.purrington@gmail.com>
2020-03-19 13:15:00 -0700

    initial commit of files

---
+++ script.js
@@ -0,0 +1,25 @@
+// Write a function that takes user input and creates a password
+// based on that input and returns it
+function generatePassword() {
+    /****
+     * WRITE YOUR CODE HERE
+     */
+  }
+  
+  //////////////////////////////////////////////////////////////
+  // DO NOT TOUCH THE CODE BELOW
+  //////////////////////////////////////////////////////////////
+  // Assignment Code
+  var generateBtn = document.querySelector("#generate");
+  
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+  
